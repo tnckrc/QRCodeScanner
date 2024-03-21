5f37e3967b45e722a192bbde09fc7355e8c963d4
tnckrc <120662119+tnckrc@users.noreply.github.com>
2024-03-21 13:30:16 +0300

    Add files via upload

---
+++ qrscanner.py
@@ -0,0 +1,22 @@
+import cv2
+import webbrowser
+
+cap = cv2.VideoCapture(0)
+
+detector = cv2.QRCodeDetector()
+while True:
+    _, img = cap.read()
+  
+    data, bbox, _ = detector.detectAndDecode(img)
+   
+    if data:
+        a=data
+        break
+  
+    cv2.imshow("QR_Scanner", img)    
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+b=webbrowser.open(str(a))
+cap.release()
+cv2.destroyAllWindows()
